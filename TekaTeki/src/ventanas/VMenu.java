5e86b0716932e448eb1a985bb4e6b7e89ddfc861
est_S <est_S@10.106.24.87>
2016-03-08 15:38:07 +0100

    Ventanas creadas

--- TekaTeki/src/ventanas/VMenu.java
+++ TekaTeki/src/ventanas/VMenu.java
@@ -6,6 +6,8 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import net.miginfocom.swing.MigLayout;
+import javax.swing.JButton;
 
 public class VMenu extends JFrame {
 
@@ -32,11 +34,27 @@ public class VMenu extends JFrame {
 	 */
 	public VMenu() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 195, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(new MigLayout("", "[grow]", "[51.00][162.00]"));
+		
+		JPanel panel = new JPanel();
+		contentPane.add(panel, "cell 0 1,alignx center,growy");
+		panel.setLayout(new MigLayout("", "[]", "[][][][]"));
+		
+		JButton btnNivelBajo = new JButton("Jugar");
+		panel.add(btnNivelBajo, "cell 0 0,alignx center");
+		
+		JButton btnNivelMedio = new JButton("Retar");
+		panel.add(btnNivelMedio, "cell 0 1,alignx center");
+		
+		JButton btnNivelDificil = new JButton("Puntuacion");
+		panel.add(btnNivelDificil, "cell 0 2,alignx center");
+		
+		JButton btnAtras = new JButton("Atras");
+		panel.add(btnAtras, "cell 0 3,alignx center");
 	}
 
 }
--- TekaTeki/src/ventanas/VNivel.java
+++ TekaTeki/src/ventanas/VNivel.java
@@ -3,9 +3,12 @@ package ventanas;
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+
+import net.miginfocom.swing.MigLayout;
 
 public class VNivel extends JFrame {
 
@@ -32,11 +35,27 @@ public class VNivel extends JFrame {
 	 */
 	public VNivel() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 195, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(new MigLayout("", "[grow]", "[51.00][162.00]"));
+		
+		JPanel panel = new JPanel();
+		contentPane.add(panel, "cell 0 1,alignx center,growy");
+		panel.setLayout(new MigLayout("", "[]", "[][][][][]"));
+		
+		JButton btnNivelBajo = new JButton("Nivel bajo");
+		panel.add(btnNivelBajo, "cell 0 1,alignx center");
+		
+		JButton btnNivelMedio = new JButton("Nivel Medio");
+		panel.add(btnNivelMedio, "cell 0 2,alignx center");
+		
+		JButton btnNivelDificil = new JButton("Nivel Dificil");
+		panel.add(btnNivelDificil, "cell 0 3,alignx center");
+		
+		JButton btnAtras = new JButton("Atras");
+		panel.add(btnAtras, "cell 0 4,alignx center");
 	}
 
 }
